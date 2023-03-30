3b5703d86fb68efb754354e19be310880ecc15b6
kenwebb <ken.webb@ns.sympatico.ca>
2023-03-30 08:35:33 -0300

    Added event handling, if else and merged testing branch

--- js/script.js
+++ js/script.js
@@ -5,5 +5,19 @@ var cat = document.querySelector(".cat");
 
 button.addEventListener("click", function () {
     //console.log("It's Working!");
-    cat.classList.add("show");
+    //cat.classList.add("show");
+    if (cat.classList.contains("show")) {
+        //console.log("Yes");
+        cat.classList.remove("show");
+        button.innerText = "Wait, come back!";
+        button.classList.add("disappear");
+    }
+    else {
+        //console.log("No");
+        cat.classList.add("show");
+        button.innerText = "Shoo, cat!";
+        button.classList.remove("disappear");
+
+    }
+
 });
